65e8131ab47681eafa05a1bf400af4c1b3a2df3a
Julian Mangual <julianmangual@Julians-MacBook-Air.local>
2025-11-11 13:01:14 -0600

    feat: Implement macOS-style close button for ModalWindow and refactor test file location

--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -10,7 +10,6 @@ const ModalWindow = ({ show, handleClose, children, title }) => {
         <div className="modal-header">
           <h5 className="modal-title">{title}</h5>
           <button type="button" className="close" onClick={handleClose}>
-            <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div className="modal-body">
